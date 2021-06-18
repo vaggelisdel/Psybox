fa130a83210fa038517b4fcf4e7400ceefbd5a43
vaggelisdel <delliosvaggelis@gmail.com>
2021-06-18 10:25:55 +0300

    upload new avatar

--- routes/Public/emailTemplate.js
+++ routes/Public/emailTemplate.js
@@ -1,23 +1,23 @@
 module.exports = {
     activation: function (token, email, hash){
-        return "<div style='background: #615dfa;padding: 3em;width: 65%;margin: 0 auto;text-align: center;'>\n" +
+        return "<div style='background: #615dfa;padding: 3em;width: 100%;margin: 0 auto;text-align: center;'>\n" +
             "    <a href='https://psybox-new.herokuapp.com'><img style='max-width: 200px;' src='https://i.imgur.com/QtqPVAE.png'/></a>\n" +
             "    <div style='background: white;padding: 2em;width: 65%;margin: 1em auto;border-radius: 20px;display: grid;'>\n" +
-            "        <img src='https://i.imgur.com/Fds2Jq4.png' style='width: 50%;margin: 0 auto;'/>\n" +
+            "        <img src='https://i.imgur.com/Fds2Jq4.png' style='width: 250px;margin: 0 auto;'/>\n" +
             "        <h2 style='width: 100%;'>Ευχαριστούμε για την εγγραφή σου!</h2>\n" +
             "        <p style='width: 100%;'>Έχετε εγγραφεί επιτυχώς αλλά χρειάζεται να επιβεβαιώσετε το λογαριασμό σας! Πατήστε στο κουμπί παρακάτω για ενεργοποίηση.</p>\n" +
-            "        <a href='https://psybox-new.herokuapp.com/verify?token="+ token +"&email="+ email +"&hash="+ hash +"' style='background: #615dfa;color: white;text-decoration: none;padding: 10px;border-radius: 10px;width: 50%;box-shadow: 1px 1px 5px 0px #00000040;margin: 2em auto;'>Επιβεβαίωση λογαριασμού</a>\n" +
+            "        <a href='https://psybox-new.herokuapp.com/verify?token="+ token +"&email="+ email +"&hash="+ hash +"' style='background: #615dfa;color: white;text-decoration: none;padding: 15px;border-radius: 10px;width: 35%;box-shadow: 1px 1px 5px 0px #00000040;margin: 2em auto;'>Επιβεβαίωση λογαριασμού</a>\n" +
             "    </div>\n" +
             "</div>";
     },
     forgotPassword: function (link) {
-        return "<div style='background: #615dfa;padding: 3em;width: 65%;margin: 0 auto;text-align: center;'>\n" +
+        return "<div style='background: #615dfa;padding: 3em;width: 100%;margin: 0 auto;text-align: center;'>\n" +
             "    <a href='https://psybox-new.herokuapp.com'><img style='max-width: 200px;' src='https://i.imgur.com/QtqPVAE.png'/></a>\n" +
             "    <div style='background: white;padding: 2em;width: 65%;margin: 1em auto;border-radius: 20px;display: grid;'>\n" +
-            "        <img src='https://i.imgur.com/wp3L8w8.png' style='width: 50%;margin: 0 auto;'/>\n" +
+            "        <img src='https://i.imgur.com/wp3L8w8.png' style='width: 250px;margin: 0 auto;'/>\n" +
             "        <h2 style='width: 100%;'>Επαναφορά κωδικού</h2>\n" +
             "        <p style='width: 100%;'>Ξέχασες τον κωδικό σου; Μην ανησυχείς! Πάτησε στο παρακάτω κουμπί για την αλλαγή του.</p>\n" +
-            "        <a href='"+ link +"' style='background: #615dfa;color: white;text-decoration: none;padding: 10px;border-radius: 10px;width: 50%;box-shadow: 1px 1px 5px 0px #00000040;margin: 2em auto;'>Αλλαγή κωδικού</a>\n" +
+            "        <a href='"+ link +"' style='background: #615dfa;color: white;text-decoration: none;padding: 15px;border-radius: 10px;width: 35%;box-shadow: 1px 1px 5px 0px #00000040;margin: 2em auto;'>Αλλαγή κωδικού</a>\n" +
             "    </div>\n" +
             "</div>";
     },

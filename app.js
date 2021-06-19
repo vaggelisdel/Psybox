301d1ee24ed5f642698e8aa465fe6f9c72bb6176
vaggelisdel <delliosvaggelis@gmail.com>
2021-06-20 00:26:27 +0300

    Exist User Modal

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var hbs = require("hbs")
+hbs.registerHelper("equal", require("handlebars-helper-equal"));
 var logger = require('morgan');
 var flash = require('connect-flash');
 const session = require('express-session');
